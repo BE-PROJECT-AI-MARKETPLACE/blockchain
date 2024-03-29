e58d5f57916f4409a27f959de88fe558f0d81e67
SameerBramhecha14 <bramhechas89@gmail.com>
2024-03-29 13:01:30 +0530

    updated contracts

--- contracts/Authentication.sol
+++ contracts/Authentication.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+import "@openzeppelin/contracts/utils/Strings.sol";
 contract Authentication{
     
     //creating a structure for user
     struct User{
+        string userID;
         string name;
         string email;
         bytes32 passwordHash;
@@ -17,6 +18,8 @@ contract Authentication{
 
     User[] public allUsers;
 
+    uint256 counter = 1;
+
     modifier onlyLoggedIn(){
         require(users[msg.sender].isLoggedIn,"User not logged in");
         _;
@@ -25,7 +28,9 @@ contract Authentication{
     function signUp(string calldata _name, string calldata _email,string calldata _password) external{
        require(!users[msg.sender].isLoggedIn,"User already registered");
        bytes32 _passwordHash = keccak256(abi.encodePacked(_password));
+       string memory _userID = Strings.toHexString(msg.sender);
        users[msg.sender] = User({
+        userID: _userID,
         name: _name,
         email:_email,
         passwordHash : _passwordHash,
@@ -33,6 +38,7 @@ contract Authentication{
        });
        emailPasswordMap[_email] = _passwordHash;
        allUsers.push(users[msg.sender]);
+       counter++;
     }
 
     function getAllUsers() external view returns(User[] memory){
@@ -58,5 +64,16 @@ contract Authentication{
 
     function isUserLoggedIn(address _user) external view returns (bool){
         return users[_user].isLoggedIn;
+    }
+
+
+    function getUserByID(string memory _userID) external view returns (User memory){
+        User memory _user ;
+        for(uint256 i=0;i<allUsers.length;i++){
+            if(keccak256(abi.encodePacked(allUsers[i].userID)) == keccak256(abi.encodePacked(_userID))){
+                _user = allUsers[i];
+            }
+        }
+        return _user;
     }
 }
